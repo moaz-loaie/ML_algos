414148a8cecca195caf0ff6bbd991e2ef6aae612
moaz-loaie <mo3azloaie@gmail.com>
2025-04-28 22:03:27 +0300

    Revert "preprocessing penguins dataset for k-means model & k-means model from scratch & k-means with sklearn & update __init__ files"

--- src/scratch/models/__init__.py
+++ src/scratch/models/__init__.py
@@ -1,7 +1,5 @@
 from .base_model import BaseModel
 from .linear_regression import LinearRegression
 from .logistic_regression import LogisticRegression
-from .K_Means import KMeans
 
-
-__all__ = ["BaseModel", "LinearRegression", "LogisticRegression", "KMeans"]
+__all__ = ["BaseModel", "LinearRegression", "LogisticRegression"]
--- src/sklearn_impl/__init__.py
+++ src/sklearn_impl/__init__.py
@@ -1,5 +1,4 @@
 from .linear_regression_sk import LinearRegressionSK
 from .logistic_regression_sk import LogisticRegressionSK
-from .k_means_sk import KMeansSK
 
-__all__ = ["LinearRegressionSK", "LogisticRegressionSK", "KMeansSK"]
+__all__ = ["LinearRegressionSK", "LogisticRegressionSK"]
