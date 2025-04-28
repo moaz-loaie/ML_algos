def695194e1978b1b7149bbcb488d73ffc38abe8
Mahmoud <star7ana@gmail.com>
2025-04-28 16:40:35 +0300

    preprocessing penguins dataset for k-means model & k-means model from scratch & k-means with sklearn & update __init__ files

--- src/scratch/models/__init__.py
+++ src/scratch/models/__init__.py
@@ -1,5 +1,7 @@
 from .base_model import BaseModel
 from .linear_regression import LinearRegression
 from .logistic_regression import LogisticRegression
+from .K_Means import KMeans
 
-__all__ = ["BaseModel", "LinearRegression", "LogisticRegression"]
+
+__all__ = ["BaseModel", "LinearRegression", "LogisticRegression", "KMeans"]
